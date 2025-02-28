a002cd8792da07a7c78522acf2307020c8979545
Mr_Durden <root@magueylabs.com>
2025-02-27 23:01:47 -0600

    Adding DB Internal IP and VPC Peering

--- main.tf
+++ main.tf
@@ -76,3 +76,19 @@ resource "google_compute_firewall" "allow_internal_db" {
 
   source_ranges = ["10.0.1.0/24"]  
 }
+
+# Internal Ip for POSTGRES
+resource "google_compute_global_address" "private_ip_alloc" {
+  name          = "google-managed-services"
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  prefix_length = 16
+  network       = google_compute_network.vpc_network.id
+}
+
+# Connect VPC - PSOTGRES
+resource "google_service_networking_connection" "private_vpc_connection" {
+  network                 = google_compute_network.vpc_network.id
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.private_ip_alloc.name]
+}
