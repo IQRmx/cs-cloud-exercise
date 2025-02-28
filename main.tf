7ac3d2341dc10e35fe1f776dcf7354ae8418b2e8
Mr_Durden <root@magueylabs.com>
2025-02-27 22:04:56 -0600

    Adding VM

--- main.tf
+++ main.tf
@@ -14,3 +14,23 @@ resource "google_compute_subnetwork" "subnet"{
 	ip_cidr_range = "10.0.1.0/24"
 	region = "us-central1"
 }
+## VM 
+resource "google_compute_instance" "vm_instance" {
+  name         = "cs-vm"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network    = google_compute_network.vpc_network.id
+    subnetwork = google_compute_subnetwork.subnet.id
+    access_config {}  
+  }
+
+  tags = ["web-server"]
+}
